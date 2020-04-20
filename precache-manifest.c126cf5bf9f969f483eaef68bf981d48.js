f158d76df1273842dd1fabea08602418651255d6
Diego Wendel <diegowendelf@gmail.com>
2020-04-19 22:34:49 -0300

    Updates

---
+++ precache-manifest.c126cf5bf9f969f483eaef68bf981d48.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91d0155512c09e63ac9e075b97cdb1c9",
+    "url": "/pipefy-clone/index.html"
+  },
+  {
+    "revision": "f4659d81e179b28942a1",
+    "url": "/pipefy-clone/static/js/2.33dfb769.chunk.js"
+  },
+  {
+    "revision": "0bdc4f0d8b3d84ae6a73bb9902b479ee",
+    "url": "/pipefy-clone/static/js/2.33dfb769.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be358616b33ad1633f73",
+    "url": "/pipefy-clone/static/js/main.56e45db3.chunk.js"
+  },
+  {
+    "revision": "a55cfc0830bc3b82d3e6",
+    "url": "/pipefy-clone/static/js/runtime-main.b880149f.js"
+  }
+]);
